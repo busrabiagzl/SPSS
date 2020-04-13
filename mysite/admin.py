ae37d5fd2dad3695bc53f4579d553f93d46cd105
busrabiagzl <60194965+busrabiagzl@users.noreply.github.com>
2020-04-13 03:21:00 +0300

    Create admin.py

---
+++ mysite/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from . import models
+
+admin.site.register(models.Calisanlar)
+admin.site.register(models.Yoneticiler)
+admin.site.register(models.Planlar)
+admin.site.register(models.Mekanlar)
